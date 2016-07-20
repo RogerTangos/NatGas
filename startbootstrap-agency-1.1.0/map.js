b51e3b52adbba2cc732c3a881e9bf7b8c83a7894
faetea <saphron.scul@gmail.com>
2016-07-20 12:12:31 -0400

    Leaflet Map in Bootstrap Template, with mock text. Would need more content and tweaking.

--- startbootstrap-agency-1.1.0/map.js
+++ startbootstrap-agency-1.1.0/map.js
@@ -1,5 +1,6 @@
-// initializes the map on the "mapid" div centered on Boston
-var mymap = L.map('mapid').setView([42.2968, -71.2924], 12);
+'use strict';
+// Instantiates the map object on the "mapid" div element.  scrollWheelZoom false, disables Whether the map can be zoomed by using the mouse wheel/trackpad, so that the map will not zoom while scrolling the webpage.
+var mymap = L.map('mapid', {scrollWheelZoom : false}).setView([42.3736, -71.1097], 12);
 
 // Mapbox Streets tile layer
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
@@ -19,7 +20,7 @@ for (var i = 0; i < leaks.length; i++) {
   var shortDate = leak['record_date'].substr(0, 4);
   var numDate = parseInt(shortDate, 10);
   var yearDiff = currentYear-numDate;
-  var radius = (yearDiff*10)+10;  // a number 10-27
+  var radius = (yearDiff*5)+5;
 
   // color by grade
   var options = { fillOpacity: 0.5 };
@@ -37,7 +38,11 @@ for (var i = 0; i < leaks.length; i++) {
     options.fillColor = '#FF00B3';
   }
 
+  // trim address quotes
+  var addressString = leak['formatted_address'];  // has quotes
+  var trim = addressString.replace( /\"/g, '');  // trims \" from begining and end of addressString
+
   // Instantiates a circle object given a geographical point, a radius in meters and optionally an options object.
   var circle = L.circle([leak['lat'], leak['lng']], radius, options).addTo(mymap);
-  circle.bindPopup('Grade: ' + leak['grade'] + '<br>' + 'Address: ' + leak['formatted_address'] + '<br>' + 'Record Date: ' + leak['record_date'] + '<br>' + 'ID: ' + leak['id']);
+  circle.bindPopup('Grade: ' + leak['grade'] + '<br>' + 'Address: ' + trim + '<br>' + 'Record Date: ' + leak['record_date'] + '<br>' + 'ID: ' + leak['id']);
 }
